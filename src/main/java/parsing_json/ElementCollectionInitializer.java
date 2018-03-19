da619b3d01b6db978659c63b45c2c61a8c1e020e
Luis Romero <luisromero@zipcoders-MacBook-Pro-112.local>
2018-03-19 13:51:18 -0400

    making some progress, testing gson in ElementCollectionInitializer

--- src/main/java/parsing_json/ElementCollectionInitializer.java
+++ src/main/java/parsing_json/ElementCollectionInitializer.java
@@ -1,17 +1,24 @@
 package parsing_json;
 
+import com.google.gson.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ElementCollectionInitializer {
+
+    public ElementCollectionInitializer() {
+    }
 
     private ArrayList<String> list = new ArrayList<String>();
     String json = new Gson().toJson(list);
@@ -27,7 +34,32 @@ public class ElementCollectionInitializer {
         Reader reader = new InputStreamReader(inputStream);
         JsonElement rootElement = parser.parse(reader);
         JsonObject rootObject = rootElement.getAsJsonObject();
+        String rootObjectString = rootElement.getAsString();
+        System.out.println(rootObjectString);
     }
 
+    public String readRawDataToString() throws Exception{
+        ClassLoader classLoader = getClass().getClassLoader();
+        String result = IOUtils.toString(classLoader.getResourceAsStream("periodic_table.json"));
+        return result;
+    }
 
+    public List<Element> convertJsonToArrayList(String jsonString) {
+        Gson gson = new Gson();
+        Type collectionType = new TypeToken<List<Element>>() {}.getType();
+        List<Element> elements = gson.fromJson(jsonString, collectionType);
+        return elements;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        ElementCollectionInitializer eci = new ElementCollectionInitializer();
+//        eci.testOnly();
+
+        String periodic_table = eci.readRawDataToString();
+//        System.out.println(periodic_table);
+        List<Element> elements = eci.convertJsonToArrayList(periodic_table);
+
+
+    }
 }

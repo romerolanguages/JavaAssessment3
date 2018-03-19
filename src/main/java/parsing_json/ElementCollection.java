827da472363219471cb4f9c85305293e00607710
Luis Romero <luisromero@zipcoders-MacBook-Pro-112.local>
2018-03-19 15:26:13 -0400

    passed tests: findByAtomicNumber, findByName

--- src/main/java/parsing_json/ElementCollection.java
+++ src/main/java/parsing_json/ElementCollection.java
@@ -4,16 +4,28 @@ import java.util.ArrayList;
 
 public class ElementCollection<Element> extends ArrayList<Element>{
 
-    public ElementCollection() {
+    public ElementCollection() throws Exception {
 
     }
 
-    public Element findByAtomicNumber(int atomic_number) {
-        return null;
+    public Element findByAtomicNumber(int atomic_number) throws Exception {
+        Element elementToReturn = null;
+        for (parsing_json.Element element : ElementCollectionInitializer.generate()) {
+            if (atomic_number == element.getNumber()) {
+                elementToReturn = (Element) element;
+            }
+        }
+        return elementToReturn;
     }
 
-    public Element findByName(String name) {
-        return null;
+    public Element findByName(String name) throws Exception {
+        Element elementToReturn = null;
+        for (parsing_json.Element element : ElementCollectionInitializer.generate()) {
+            if (name.equals(element.getName())) {
+                elementToReturn = (Element) element;
+            }
+        }
+        return elementToReturn;
     }
 
     public ElementCollection<Element> where(String fieldName, Object value) {

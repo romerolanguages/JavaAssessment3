1d537f19438236549a978b12fe7979191d458946
Luis Romero <luisromero@zipcoders-MacBook-Pro-112.local>
2018-03-19 16:47:23 -0400

    finished section 1, all tests pass

--- src/main/java/parsing_json/Element.java
+++ src/main/java/parsing_json/Element.java
@@ -1,30 +1,31 @@
 package parsing_json;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class Element {
 
-    private String name;
-    private String appearance;
-    private double atomic_mass;
-    private double boil;
-    private String category;
-    private String color;
-    private double density;
-    private String discovered_by;
-    private double melt;
-    private double molar_heat;
-    private String named_by;
-    private int number;
-    private int period;
-    private String phase;
-    private String source;
-    private String spectral_img;
-    private String summary;
-    private String symbol;
-    private int xpos;
-    private int ypos;
-    private ArrayList<Integer> shells;
+    protected String name;
+    protected String appearance;
+    protected double atomic_mass;
+    protected double boil;
+    protected String category;
+    protected String color;
+    protected double density;
+    protected String discovered_by;
+    protected double melt;
+    protected double molar_heat;
+    protected String named_by;
+    protected int number;
+    protected int period;
+    protected String phase;
+    protected String source;
+    protected String spectral_img;
+    protected String summary;
+    protected String symbol;
+    protected int xpos;
+    protected int ypos;
+    protected ArrayList<Integer> shells;
 
     public Element() {
     }
@@ -140,5 +141,12 @@ public class Element {
         return shells;
     }
 
+    public static void main(String[] args) throws NoSuchFieldException {
+
+        Field field = Element.class.getDeclaredField("name");
+
+        System.out.println(field.toString());
+
+    }
 
 }

7f6696ff647203246c281dac893707ce8088791c
Luis Romero <luisromero@zipcoders-MacBook-Pro-112.local>
2018-03-19 17:05:44 -0400

    building two constructors

--- src/main/java/user_management/User.java
+++ src/main/java/user_management/User.java
@@ -4,8 +4,27 @@ import user_management.security.Password;
 
 public class User {
 
+    protected String email;
+    protected String id;
+    protected String name;
+    protected String passwordAsString;
+    protected Password passwordAsPassword;
+
     public User() {
-        
+    }
+
+    public User(String email, String id, String name, Password password) {
+        this.email = email;
+        this.id = id;
+        this.name = name;
+        this.passwordAsPassword = password;
+    }
+
+    public User(String email, String id, String name, String password) {
+        this.email = email;
+        this.id = id;
+        this.name = name;
+        this.passwordAsString = password;
     }
 
     @Override

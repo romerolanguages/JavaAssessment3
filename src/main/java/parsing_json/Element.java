0e9fb9b57a643fab7345aa4a053ad4c61b24dc08
Luis Romero <luisromero@zipcoders-MacBook-Pro-112.local>
2018-03-19 10:22:33 -0400

    added all fields in Element class

--- src/main/java/parsing_json/Element.java
+++ src/main/java/parsing_json/Element.java
@@ -1,4 +1,38 @@
 package parsing_json;
 
+import java.util.ArrayList;
+
 public class Element {
+
+    private String name;
+    private String appearance;
+    private double atomic_mass;
+    private double boil;
+    private String category;
+    private String color;
+    private double density;
+    private String discovered_by;
+    private double melt;
+    private double molar_heat;
+    private String named_by;
+    private int number;
+    private int period;
+    private String phase;
+    private String source;
+    private String spectral_img;
+    private String summary;
+    private String symbol;
+    private int xpos;
+    private int ypos;
+    private ArrayList<Integer> shells;
+
+    public Element() {
+
+    }
+
+//    public Element() {
+//
+//    }
+
+
 }

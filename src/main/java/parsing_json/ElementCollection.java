ee7359e610728d7705bd932f2eba400591b23fbf
Luis Romero <luisromero@zipcoders-MacBook-Pro-112.local>
2018-03-19 16:19:11 -0400

    last method not working

--- src/main/java/parsing_json/ElementCollection.java
+++ src/main/java/parsing_json/ElementCollection.java
@@ -1,5 +1,6 @@
 package parsing_json;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class ElementCollection<Element> extends ArrayList<Element>{
@@ -28,7 +29,20 @@ public class ElementCollection<Element> extends ArrayList<Element>{
         return elementToReturn;
     }
 
-    public ElementCollection<Element> where(String fieldName, Object value) {
-        return null;
+    public ElementCollection<Element> where(String fieldName, Object value) throws Exception {
+        ElementCollection<Element> elementsThatMatch = new ElementCollection<>();
+
+        Class elementClassInQuestion = parsing_json.Element.class;
+        
+        Field field = elementClassInQuestion.getField(fieldName);
+
+        //if (field != null) {
+            for (parsing_json.Element element : ElementCollectionInitializer.generate()) {
+                if (field.get(element).equals(value) || field.get(element) == value) {
+                    elementsThatMatch.add((Element) element);
+                }
+            }
+        //}
+        return elementsThatMatch;
     }
 }

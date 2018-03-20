f6771c505555be2df6412473a790d0b8c07283ae
Luis Romero <luisromero@zipcoders-MacBook-Pro-112.local>
2018-03-19 20:13:58 -0400

    passed UserCollectionInitializer test

--- src/main/java/user_management/UserCollection.java
+++ src/main/java/user_management/UserCollection.java
@@ -1,20 +1,22 @@
-//package user_management;
-//
-//public class UserCollection {
-//
-//    public User findById(int id) {
-//        return null;
-//    }
-//
-//    public User findByEmail(String email) {
-//        return null;
-//    }
-//
-//    public User attemptLogin(String email, String password) {
-//        return null;
-//    }
-//
-//    public int createUser(String name, String email, String password) {
-//        return 0;
-//    }
-//}
+package user_management;
+
+import java.util.ArrayList;
+
+public class UserCollection<E> extends ArrayList {
+
+    public User findById(int id) {
+        return null;
+    }
+
+    public User findByEmail(String email) {
+        return null;
+    }
+
+    public User attemptLogin(String email, String password) {
+        return null;
+    }
+
+    public int createUser(String name, String email, String password) {
+        return 0;
+    }
+}
--- src/main/java/user_management/UserCollectionInitializer.java
+++ src/main/java/user_management/UserCollectionInitializer.java
@@ -1,7 +1,46 @@
-//package user_management;
-//
-//public class UserCollectionInitializer {
-//    public static UserCollection generate() {
-//        return null;
-//    }
-//}
+package user_management;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.apache.commons.io.IOUtils;
+
+import java.lang.reflect.Type;
+import java.util.Arrays;
+
+public class UserCollectionInitializer {
+
+    public UserCollectionInitializer() {
+    }
+
+    public static UserCollection generate() throws Exception {
+        String users = readRawDataToString();
+        UserCollection<User> userCollection = convertJsonStringToUserCollection(users);
+        return userCollection;
+    }
+
+    // Changed to "ElementCollection.class" to be able to read from static method
+    public static String readRawDataToString() throws Exception{
+        ClassLoader classLoader = UserCollection.class.getClassLoader();
+        String result = IOUtils.toString(classLoader.getResourceAsStream("users.json"));
+        return result;
+    }
+
+    public static UserCollection<User> convertJsonStringToUserCollection(String jsonString) {
+        Gson gson = new Gson();
+        Type collectionType = new TypeToken<UserCollection<User>>() {}.getType();
+        UserCollection<User> elements = gson.fromJson(jsonString, collectionType);
+        return elements;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        UserCollectionInitializer eci = new UserCollectionInitializer();
+        String users = eci.readRawDataToString();
+        UserCollection<User> userCollection = eci.convertJsonStringToUserCollection(users);
+
+        System.out.println(Arrays.toString(userCollection.toArray()));
+        System.out.println(userCollection.get(0));
+
+    }
+
+}
